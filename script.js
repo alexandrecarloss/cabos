dca91d336711d14eed3a8b8c3517785990d32a71
alexandrecarloss <107065086+alexandrecarloss@users.noreply.github.com>
2023-05-29 12:26:33 -0400

    Nível 7 criado fino ou médio

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Objetivo: criar um sistema que receba as características de cabo do usuário e identifique quais cabos da lista atendem aos requisitos
+
 function cabo() {
     //Função que é ativada ao clicar no botão verificar, ela deve receber as características e identificar o cabo.
     
@@ -10,10 +11,10 @@ function cabo() {
     var blindagem = document.querySelector('input[name="blindagem"]:checked').value;
     var cor = document.querySelector('input[name="cor"]:checked').value;
     var rigidez = document.querySelector('input[name="rigidez"]:checked').value;
-    var espessura = parseFloat(document.getElementById("iespessura").value);
+    var espessura = document.querySelector('input[name="espessura"]:checked').value;
 
     //Vetor que vai conter as características
-    var cabo = [8]; 
+    var cabo = []; 
 
     //Lista dos tipos de cabo
    // var lista = ["USB", "Paralelo e Serial", "Monomodo", "Multimodo", "Traçado (STP)", "Traçado (UTP)", "Thicknet", "Thinnet"];
@@ -29,6 +30,8 @@ function cabo() {
     var nivel4 = [];
     var nivel5 = [];
     var nivel6 = [];
+    var nivel7 = [];
+    // Níveis de hierarquia primeiramente recebem todos os cabos da lista
    for (var i = 0; i <= 7; i++) {
         nivel1[i] = lista[i];
         nivel2[i] = lista[i];
@@ -36,36 +39,32 @@ function cabo() {
         nivel4[i] = lista[i];
         nivel5[i] = lista[i];
         nivel6[i] = lista[i];
+        nivel7[i] = lista[i];
    }
    
-   
+   //Lista final, se tiver deve conter os cabos que passaram por todas as características
    var final = [];
-
-    // lista.set("0", "USB");
-    // lista.set("1", "Paralelo e Serial");
-    // lista.set("2", "Monomodo");
-    // lista.set("3", "Multimodo");
-    // lista.set("4", "Traçado (STP)");
-    // lista.set("5", "Traçado (UTP)");
-    // lista.set("6", "Thicknet");
-    // lista.set("7", "Thinnet");
-    //delete lista[1];
-    // for (let [key, value] of lista) {
-    //    window.alert(key + " tem valor " + value);
-    // }
     
-
-    //Listas de cada cabo
-    var usb = [480, "Multiuso", 2, 10, "Sem blindagem", cor, "Flexível", 0.8];
-    var paralelo = [0.45, "Multiuso", 1.5, 5, "Sem blindagem", cor, "Flexível", 1.3];
-    var monomodo = [40000, "Sem multiuso", 100000, 8, "Vidro", cor, "Flexível", 0.001];
-    var multimodo = [100, "Sem multiuso", 2000, 7, "Vidro", cor, "Flexível", 0.0062];
-    var tracadostp = [10000, "Sem multiuso", 100, 14, "Metal", cor, "Rígido", 0.7];
+    /*Listas de cada cabo com suas características na ordem
+   Velocidade 
+   Multiuso
+   Comprimento
+   Preço
+   Blindagem
+   cor: a cor não é calculada, pois o cabo assume qualquer cor que o usuário escolher
+   Rigidez
+   Espessura
+    */
+    var usb = [480, "Multiuso", 2, 10, "Sem blindagem", cor, "Flexível", "Fino"];
+    var paralelo = [0.45, "Multiuso", 1.5, 5, "Sem blindagem", cor, "Flexível", "Médio"];
+    var monomodo = [40000, "Sem multiuso", 100000, 8, "Vidro", cor, "Flexível", "Fino"];
+    var multimodo = [100, "Sem multiuso", 2000, 7, "Vidro", cor, "Flexível", "Fino"];
+    var tracadostp = [10000, "Sem multiuso", 100, 14, "Metal", cor, "Rígido", "Fino"];
     var tracadoutp = [10000, "Sem multiuso", 100, 9, "Sem blindagem", cor, "Flexível", 0.5];
-    var thicknet = [10, "Sem multiuso", 500, 13, "Metal", cor, "Rígido", 1];
-    var thinnet = [10, "Sem multiuso", 185, 15, "Metal", cor, "Flexível", 0.64];
+    var thicknet = [10, "Sem multiuso", 500, 13, "Metal", cor, "Rígido", "Médio"];
+    var thinnet = [10, "Sem multiuso", 185, 15, "Metal", cor, "Flexível", "Fino"];
 
-    //Substituição das variáveis nulas por 0
+    //Substituição das variáveis de valor numérico nulas por 0
     if(isNaN(velocidade)) {
         velocidade = 0;
     }
@@ -75,11 +74,8 @@ function cabo() {
     if(isNaN(preco)) {
         preco = 0;
     }
-    if(isNaN(espessura)) {
-        espessura = 0;
-    }
     
-    //Atribuição das características para o vetor cabo
+    //Atribuição das características para o vetor cabo que vai ser usado para comparar com cada um dos cabos
     cabo[0] = velocidade;
     cabo[1] = multiuso;
     cabo[2] = distancia;
@@ -89,7 +85,7 @@ function cabo() {
     cabo[6] = rigidez;
     cabo[7] = espessura;
 
-    //Verificação nível 1: velocidade
+    //Verificação nível 1: velocidade os que não atendem saem da lista nivel1 
     if(cabo[0] > usb[0]) {
         nivel1.splice(nivel1.indexOf("USB"), 1);
     }
@@ -218,12 +214,25 @@ function cabo() {
         nivel6.splice(nivel6.indexOf("Traçado (STP)"), 1);
         nivel6.splice(nivel6.indexOf("Thicknet"), 1);
     }
-    
-    for (var i = 0; i <= 7; i++) {
-        if (nivel1.indexOf("USB") == 0 ) {
-            
-        }
+
+    if (cabo[7] == "Fino") {
+        nivel7.splice(nivel7.indexOf("Paralelo e Serial"), 1);
+        nivel7.splice(nivel7.indexOf("Thicknet"), 1);
     }
+    if (cabo[7] == "Médio") {
+        nivel7.splice(nivel7.indexOf("USB"), 1);
+        nivel7.splice(nivel7.indexOf("Monomodo"), 1);
+        nivel7.splice(nivel7.indexOf("Multimodo"), 1);
+        nivel7.splice(nivel7.indexOf("Traçado (UTP)"), 1);
+        nivel7.splice(nivel7.indexOf("Traçado (STP)"), 1);
+        nivel7.splice(nivel7.indexOf("Thinnet"), 1);
+    }
+    
+    // for (var i = 0; i <= 7; i++) {
+    //     if (nivel1.indexOf("USB") == 0 ) {
+            
+    //     }
+    // }
     //final = nivel1.get("0");
 
     
@@ -270,6 +279,7 @@ function cabo() {
     window.alert("4: " + nivel4);
     window.alert("5: " + nivel5);
     window.alert("6: " + nivel6);
+    window.alert("7: " + nivel7);
     //window.alert(typeof(nivel1));
     // i = [1, "a"]
     // i.forEach(function (item, indice, array) {
